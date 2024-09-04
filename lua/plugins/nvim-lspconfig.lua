92e662347968b0ada0b6660dced261cca4bc8a01
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2024-09-05 02:14:43 +0300

    void: Add Mason to install LSPs

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,11 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
+    require("mason").setup()
+
+    require("mason-lspconfig").setup()
+
     local lspconfig = require("lspconfig")
-
-
 
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('UserLspConfig', {}),
